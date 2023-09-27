8cc82004dbf3a5674cb57b725fe1a60eec91da97
gajendra <davegk59@gmil.com>
2023-09-27 08:47:48 +0530

    added comments features

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200) 
+    title = models.CharField(max_length=255) 
     slug = models.SlugField()
     intro = models.TextField()
     body = models.TextField()
@@ -13,10 +13,10 @@ class Post(models.Model):
         
 class Comment(models.Model):
     post =  models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=255)
     email = models.EmailField()
     body = models.TextField()
-    date_added = models.DateTimeField(auto_new_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
     
     class Meta:
         ordering = ['date_added'] 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+
+from .forms import CommentForm 
 from .models import Post 
 
 # Create your views here.
@@ -7,8 +9,8 @@ def frontpage(request):
     posts = Post.objects.all()
     return render(request,'blog/frontpage.html',{'posts':posts})
 
-def post_detail(request):
-    post = Post.objects.get(slug=post.slug)
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
     
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -16,7 +18,7 @@ def post_detail(request):
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
-            commet.save()
+            comment.save()
             
             return redirect('post_detail', slug=post.slug)
         
